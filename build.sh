fc2743175f12ce66ade289fd328c942ba2c8adac
douglas.bullard <douglas.bullard@0c8f0584-c752-11dd-bfe9-7982af55821d>
2009-05-04 06:05:40 +0000

    Moving to new buildscripts

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 #set JAVA_HOME=/usr
 #solaris
 
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home/
+JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/
 
 #set PATH=$PATH:$JAVA_HOME/bin
 export PATH
